f7ecb79800a277735c5c2b0f733f5b388f248728
Viktor Bilonozhko <viktorbilonozhko2005@gmail.com>
2024-05-31 01:02:24 +0300

    New history for motobike

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link href="css/reset.css" rel="stylesheet">
+    <link href="css/index.css" rel="stylesheet">
+    <link rel="shortcut icon" href="image/logo.png" type="image/png">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MotoInfoCenter.com</title>
+</head>
+<body>
+    <div class="block-body">
+        <?php include("modules/header.php");?>
+        <div class="block-content">
+        <?php include("modules/block-left.php");?>
+        <div class="content"></div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ modules/header.php
@@ -0,0 +1,14 @@
+<header>
+    <div class="block-header">
+        <div class="header-logo"><a href="#" class="logo"><img src="../image/logo.png" alt="logo"></a></div>
+        <div class="header-menu">
+            <nav class="menu-body">
+                <ul class="menu-list">
+                    <li><a href="#">Про нас</a></li>
+                    <li><a href="#">Контакти</a></li>
+                    <li><a href="#">Вхід</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</header>
